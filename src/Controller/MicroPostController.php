9020384e2ca7620dbe77a13bc6593d39d6607c87
Bogdan Vlad <vlad.bogdan90@gmail.com>
2019-01-21 22:59:16 +0100

    created a form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\MicroPostRepository;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\MicroPost;
+use App\Form\MicroPostType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/micro-post")
@@ -20,11 +24,18 @@ class MicroPostController
      * @var MicroPostRepository
      */
     private $microPostRepository;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
 
-    public function __construct(\Twig_Environment $twig, MicroPostRepository $microPostRepository)
-    {
+    public function __construct(
+        \Twig_Environment $twig, MicroPostRepository $microPostRepository,
+        FormFactoryInterface $formFactory
+    ) {
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
+        $this->formFactory = $formFactory;
     }
 
     /**
@@ -36,5 +47,28 @@ class MicroPostController
             'posts' => $this->microPostRepository->findAll()
         ]);
         return new Response($html);
+    }
+
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+        // dump($formFactory);
+        $microPost = new MicroPost();
+        $microPost->setTime(new \DateTime());
+
+        $form = $this->formFactory->create(MicroPostType::class, $microPost);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
+        return new Response(
+            $this->twig->render(
+                'micro-post/add.html.twig',
+                ['form' => $form->createView()]
+            )
+        );
     }
 }
