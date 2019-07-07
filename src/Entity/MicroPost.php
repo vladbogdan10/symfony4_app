45e0db6f34147fee0355df49d6d8c08594c10a70
Vlad Bogdan <vlad.bogdan90@gmail.com>
2019-07-07 13:10:47 +0200

    added assertions to the post body

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ * @ORM\Table()
  */
 class MicroPost
 {
@@ -18,6 +20,8 @@ class MicroPost
 
     /**
      * @ORM\Column(type="string", length=280)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="10")
      */
     private $text;
 
