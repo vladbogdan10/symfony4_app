3dbc3c4be9a5e6d82f8651472424bbec6598086d
Vlad Bogdan <vlad.bogdan90@gmail.com>
2019-07-14 15:00:26 +0200

    added user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,21 +2,57 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\MicroPost;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadMicroPost($manager);
+        $this->loadUsers($manager);
+    }
+
+    private function loadMicroPost(ObjectManager $manager)
     {
         for ($i = 0; $i < 10; $i++) {
             $microPost = new MicroPost();
             $microPost->setText('Some random text ' . rand(0, 100));
-            $microPost->setTime(new \DateTime('2019-07-07'));
+            $microPost->setTime(new DateTime('2019-07-07'));
             $manager->persist($microPost);
         }
 
+        $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('john_doe');
+        $user->setFullName('John Doe');
+        $user->setEmail('john_doe@doe.com');
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword(
+                $user, 'john123'
+            )
+        );
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
